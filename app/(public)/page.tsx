5859b900ed0269e64330468ebd7c92a353dd7bbc
avilacrew369 <184137556+avilacrew369@users.noreply.github.com>
2026-03-08 13:49:28 -0500

    segunda

---
+++ app/(public)/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Meeti - Inicio'
+};
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>Incio</h1>
+    </>
+  
+  );
+}
